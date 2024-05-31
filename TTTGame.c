d21a2c6cf943d7ab2fefbab6a0bf7f674682e54d
Kael <mhenokh@gmail.com>
2024-05-31 21:41:41 +0700

    Added Single and Multi Mode, Scoring Function + File Handling (Score History)

--- TTTGame.c
+++ TTTGame.c
@@ -4,13 +4,15 @@
 #include <conio.h>
 #include <windows.h>
 
-// Define the Node structure
+// Define struct
 typedef struct Node {
     char data;
     struct Node* next;
 } Node;
 
-// Function to create a new node
+int playerOneScore = 0, playerTwoScore = 0, botScore = 0;
+
+// New node function
 Node* createNode(char data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
@@ -32,13 +34,13 @@ void displayBoard(Node* head) {
     printf("\n");
 }
 
-// Function to check if there is a win
+// Function to check if there is a win condition (Horizontal, Vertical, or Diagonal)
 int checkWin(Node* head) {
     char board[3][3];
     int k = 0;
     Node* current = head;
     
-    // Convert linked list to 2D array for easy checking
+    // Convert linked list to 2D array
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             board[i][j] = current->data;
@@ -46,13 +48,13 @@ int checkWin(Node* head) {
         }
     }
 
-    // Check rows and columns
+    // Check for Horizontal and Vertical (Row and Columns)
     for (int i = 0; i < 3; i++) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') return 1;
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') return 1;
     }
 
-    // Check diagonals
+    // Check Diagonals
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') return 1;
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') return 1;
 
@@ -60,7 +62,7 @@ int checkWin(Node* head) {
 }
 
 // Function to check if the board is full
-int isBoardFull(Node* head) {
+int checkFullBoard(Node* head) {
     while (head != NULL) {
         if (head->data == ' ') return 0;
         head = head->next;
@@ -68,11 +70,29 @@ int isBoardFull(Node* head) {
     return 1;
 }
 
-void game() {
+// Function for file processing (Score history)
+void scoreFile(int score1, int score2, int mode){
+    time_t t = time(NULL); // Gets the current callendar time. NULL because we are not giving a variable to restore the result
+    struct tm tm = *localtime(&t); // Converts the time_t val into a better format (yyyy/mm/dd)
+    FILE *file = fopen("Scores.txt", "a"); // Open Scores.txt and set to append
+    if(file == NULL){
+        printf("Unable to access score file!\n");
+        return;
+    }
+    if(mode == 1){
+        fprintf(file, "%04d-%02d-%02d - Player 1 VS Bot = %d : %d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, score1, score2); // Scores format (Date - Player 1 VS Bot = Score 1 : Score 2)
+    } else{
+        fprintf(file, "%04d-%02d-%02d - Player 1 VS Player 2 = %d : %d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, score1, score2); // Scores format (Date - Player 1 VS Player 2 = Score 1 : Score 2)
+    }
+    fclose(file);
+}
+
+// Function to play the game
+void TicTacToe(int gameMode) {
     Node* head = NULL;
     Node* current = NULL;
 
-    // Initialize the linked list representing the game board
+    // Initialize the linked list to represent the game board
     for (int i = 0; i < 9; i++) {
         if (head == NULL) {
             head = createNode(' ');
@@ -83,10 +103,11 @@ void game() {
         }
     }
 
-    srand(time(NULL)); // Seed the random number generator
+    srand(time(NULL)); // Seed the RNG for bot
 
     int currentPlayer = 1;
     int gameOver = 0;
+    int playAgain = 0;
     int move;
     char mark;
 
@@ -97,17 +118,22 @@ void game() {
             printf("Player 1, enter a number (1-9) to place X: ");
             scanf("%d", &move);
         } else {
-            do {
-                move = rand() % 9 + 1; // Bot chooses a random move
-                current = head;
-                for (int i = 1; i < move; i++) {
-                    current = current->next;
-                }
-            } while (current->data != ' ');
-            printf("Bot chooses position %d\n", move);
+            if (gameMode == 1) {
+                do {
+                    move = rand() % 9 + 1; // Bot RNG Move
+                    current = head;
+                    for (int i = 1; i < move; i++) {
+                        current = current->next;
+                    }
+                } while (current->data != ' ');
+                printf("Bot chooses position %d\n", move);
+            } else {
+                printf("Player 2, enter a number (1-9) to place O: ");
+                scanf("%d", &move);
+            }
         }
 
-        mark = (currentPlayer == 1) ? 'X' : 'O';
+        mark = (currentPlayer == 1) ? 'X' : 'O'; // Check if the currentPlayer is 1, if yes it will mark 'X', if not it will mark 'O'
 
         current = head;
         for (int i = 1; i < move; i++) {
@@ -115,15 +141,22 @@ void game() {
         }
         if (current->data == ' ') {
             current->data = mark;
-            if (checkWin(head)) {
+            if (checkWin(head)) { // If someone wins the game
                 displayBoard(head);
                 if (currentPlayer == 1) {
                     printf("Player 1 wins!\n");
+                    playerOneScore++;
                 } else {
-                    printf("Bot wins!\n");
+                    if(gameMode == 1){
+                        printf("Bot wins!\n");
+                        botScore++;
+                    } else{
+                        printf("Player 2 Wins!\n");
+                        playerTwoScore++;
+                    }
                 }
                 gameOver = 1;
-            } else if (isBoardFull(head)) {
+            } else if (checkFullBoard(head)) {
                 displayBoard(head);
                 printf("The game is a draw.\n");
                 gameOver = 1;
@@ -140,9 +173,23 @@ void game() {
         free(current);
         current = next;
     }
-}
 
+    printf("Do you want to play again?(1 for Yes | 0 for No)\n");
+    scanf("%d", &playAgain);
+    if (playAgain == 1) {
+        TicTacToe(gameMode);
+    } else {
+        if(gameMode == 1){
+            scoreFile(playerOneScore, botScore, gameMode);
+        }else{
+            scoreFile(playerOneScore, playerTwoScore, gameMode);
+        }
+        return;
+    }
+}
+ // Menu function
 void menu(){
+    system("CLS");
     printf("  _______         \n"
            " /_  ___/_____    \n"
            "  / / / / ___/    \n"
@@ -157,40 +204,82 @@ void menu(){
            " /_  __/___  ___  \n"
            "  / / / __ \\/ _ \\ \n"
            " / / / /_/ /  __/ \n"
-           "/_/  \\____/\\___/\n");
+           "/_/  \\____/\\___/\n"); // The game title in ASCII art
 
     printf("\n1. Play Game\n");
     printf("2. View Score\n");
-    printf("3. Exit\n\n\n\n");
-    printf("Please pick a choice");
+    printf("0. Exit\n\n\n\n");
+    printf("Please pick a choice\n");
 }
 
+// Main function
 int main() {
-    int choice;
+    int choice, gameChoice;
+    start:
     menu();
-    while(choice > 3 || choice <= 0){
+    do{
         printf("\t>>");
         scanf("%d", &choice);
-    }
+    }while(choice > 3);
+
     switch(choice){
         case 1:
+            system("CLS"); // To make the screen clear
+            printf("Please pick a gamemode!\n\n");
+            printf("1. Single Player (VS Bot)\n");
+            printf("2. Multi Plater (PVP)\n");
+            printf("0. Back\n");
+            do{
+                printf("\t>>");
+                scanf("%d", &gameChoice);
+            }while(gameChoice < 0 || gameChoice > 2);
+            switch(gameChoice){
+                case 1:
+                    TicTacToe(1);
+                    goto start;
+                break;
+                case 2:
+                    TicTacToe(2);
+                    goto start;
+                break;
+                case 0:
+                    goto start;
+                break;
+            }
         break;
         case 2:
-        break;
-        case 3:
+            system("CLS");
+            FILE *file = fopen("Scores.txt", "r");
+            if (file == NULL) {
+                printf("Unable to open scores file.\n");
+                break;
+            }
 
-        printf( "  ______________  ___  ________  ________   ___  __             ___    ___ ________  ___  ___          ________ ________  ________          ________  ___       ________      ___    ___ ___  ________   ________     \n"
-                " |\\___   __\\ \\  \\|\\  \\|\\   __  \\|\\   ___  \\|\\  \\|\\  \\          |\\  \\  /  /|\\   __  \\|\\  \\|\\  \\        |\\  _____\\\\   __  \\|\\   __  \\        |\\   __  \\|\\  \\     |\\   __  \\    |\\  \\  /  /|\\  \\|\\   ___  \\|\\   ____\\    \n"
-                " \\|___ \\  \\ \\ \\  \\_\\  \\ \\  \\_\\  \\ \\  \\\\ \\  \\ \\  \\/  /__        \\ \\  \\/  / | \\  \\ \\  \\ \\  \\ \\  \\       \\ \\  \\___\\ \\  \\ \\  \\ \\  \\_\\  \\       \\ \\  \\_\\  \\ \\  \\    \\ \\  \\_\\  \\   \\ \\  \\/  / | \\  \\ \\  \\\\ \\  \\ \\  \\___|    \n"
-                "      \\ \\  \\ \\ \\   __  \\ \\   __  \\ \\  \\\\ \\  \\ \\   ___  \\        \\ \\    / / \\ \\  \\ \\  \\ \\  \\ \\  \\       \\ \\   __\\\\ \\  \\ \\  \\ \\   _  _\\       \\ \\   ____\\ \\  \\    \\ \\   __  \\   \\ \\    / / \\ \\  \\ \\  \\\\ \\  \\ \\  \\  ___  \n"
-                "       \\ \\  \\ \\ \\  \\ \\  \\ \\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\\\ \\  \\        \\/  /  /   \\ \\  \\_\\  \\ \\  \\_\\  \\       \\ \\  \\_| \\ \\  \\_\\  \\ \\  \\\\  \\        \\ \\  \\___|\\ \\  \\____\\ \\  \\ \\  \\   \\/  /  /   \\ \\  \\ \\  \\\\ \\  \\ \\  \\_\\  \\ \n"
-                "        \\ \\__\\ \\ \\__\\ \\__\\ \\__\\ \\__\\ \\__\\\\ \\__\\ \\__\\\\ \\__\\     __/  / /      \\ \\_______\\ \\_______\\       \\ \\__\\   \\ \\_______\\ \\__\\\\ _\\        \\ \\__\\    \\ \\_______\\ \\__\\ \\__\\__/  / /      \\ \\__\\ \\__\\\\ \\__\\ \\_______\\\n"
-                "         \\|__|  \\|__|\\|__|\\|__|\\|__|\\|__| \\|__|\\|__| \\|__|    |\\___/ /        \\|_______|\\|_______|        \\|__|    \\|_______|\\|__|\\|__|        \\|__|     \\|_______|\\|__|\\|__|\\___/ /        \\|__|\\|__| \\|__|\\|_______|\n"
-                "                                                              \\|___|/                                                                                                       \\|___|/\n");
-        sleep(2);
-        return 0;
+            char line[256];
+            while (fgets(line, sizeof(line), file)) { // Write all 'line' in Scores.txt
+                printf("%s", line);
+            }
+            fclose(file);
+
+            printf("\nPress any key to go back.\n");
+            getch();
+            goto start;
+        break;
+        case 0:
+            system("CLS");
+            // vvv basically just 'THANK YOU FOR PLAYING' in ASCII art and 3D vvv
+            printf( "  ______________  ___  ________  ________   ___  __             ___    ___ ________  ___  ___          ________ ________  ________          ________  ___       ________      ___    ___ ___  ________   ________     \n"
+                    " |\\___   __\\ \\  \\|\\  \\|\\   __  \\|\\   ___  \\|\\  \\|\\  \\          |\\  \\  /  /|\\   __  \\|\\  \\|\\  \\        |\\  _____\\\\   __  \\|\\   __  \\        |\\   __  \\|\\  \\     |\\   __  \\    |\\  \\  /  /|\\  \\|\\   ___  \\|\\   ____\\    \n"
+                    " \\|___ \\  \\ \\ \\  \\_\\  \\ \\  \\_\\  \\ \\  \\\\ \\  \\ \\  \\/  /__        \\ \\  \\/  / | \\  \\ \\  \\ \\  \\ \\  \\       \\ \\  \\___\\ \\  \\ \\  \\ \\  \\_\\  \\       \\ \\  \\_\\  \\ \\  \\    \\ \\  \\_\\  \\   \\ \\  \\/  / | \\  \\ \\  \\\\ \\  \\ \\  \\___|    \n"
+                    "      \\ \\  \\ \\ \\   __  \\ \\   __  \\ \\  \\\\ \\  \\ \\   ___  \\        \\ \\    / / \\ \\  \\ \\  \\ \\  \\ \\  \\       \\ \\   __\\\\ \\  \\ \\  \\ \\   _  _\\       \\ \\   ____\\ \\  \\    \\ \\   __  \\   \\ \\    / / \\ \\  \\ \\  \\\\ \\  \\ \\  \\  ___  \n"
+                    "       \\ \\  \\ \\ \\  \\ \\  \\ \\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\\\ \\  \\        \\/  /  /   \\ \\  \\_\\  \\ \\  \\_\\  \\       \\ \\  \\_| \\ \\  \\_\\  \\ \\  \\\\  \\        \\ \\  \\___|\\ \\  \\____\\ \\  \\ \\  \\   \\/  /  /   \\ \\  \\ \\  \\\\ \\  \\ \\  \\_\\  \\ \n"
+                    "        \\ \\__\\ \\ \\__\\ \\__\\ \\__\\ \\__\\ \\__\\\\ \\__\\ \\__\\\\ \\__\\     __/  / /      \\ \\_______\\ \\_______\\       \\ \\__\\   \\ \\_______\\ \\__\\\\ _\\        \\ \\__\\    \\ \\_______\\ \\__\\ \\__\\__/  / /      \\ \\__\\ \\__\\\\ \\__\\ \\_______\\\n"
+                    "         \\|__|  \\|__|\\|__|\\|__|\\|__|\\|__| \\|__|\\|__| \\|__|    |\\___/ /        \\|_______|\\|_______|        \\|__|    \\|_______|\\|__|\\|__|        \\|__|     \\|_______|\\|__|\\|__|\\___/ /        \\|__|\\|__| \\|__|\\|_______|\n"
+                    "                                                              \\|___|/                                                                                                       \\|___|/\n");
+            printf("Press anything to quit...");
+            getch();
+            return 0;
         break;
     }
-    game();
     return 0;
 }
